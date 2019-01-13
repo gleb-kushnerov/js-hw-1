c51573de55d17ae50eedd305595ec825435c58d4
Che1sea57 <38821256+Che1sea57@users.noreply.github.com>
2019-01-13 20:02:53 +0300

    сделал функции для ввода имени, фамилии, возраста и пола

--- task-2.js
+++ task-2.js
@@ -1,4 +1,76 @@
 (function () {
     'use strict';
 
+    let name,
+        surname,
+        age,
+        gender,
+        pension;
+
+    function userName() {
+        let userInputName = prompt('Введите ваше имя');
+        if (userInputName === null || userInputName === '') {
+            userName();
+        } else {
+            name = userInputName;
+        }
+        return name;
+    }
+
+    function userSurname() {
+        let userInputSurname = prompt('Введите вашу фамилию');
+        if (userInputSurname === null || userInputSurname === '') {
+            userSurname();
+        } else {
+            surname = userInputSurname;
+        }
+        return surname;
+    }
+
+    function userGender() {
+        let userInputGender = prompt('Введите ваш пол: М или Ж');
+        if (userInputGender === 'м' || userInputGender === 'ж'|| userInputGender === 'М' || userInputGender === 'Ж') {
+            gender = userInputGender;
+            return gender
+        } else {
+            userGender();
+        }
+    }
+
+
+    function userAge() {
+        let userInputAgeStr = prompt('Введите ваш возраст');
+        let userInputAgeNumber = (parseFloat(userInputAgeStr.trim()));
+        if (isFinite(userInputAgeNumber) && userInputAgeNumber > 0) {
+            age = userInputAgeNumber;
+            return age;
+        } else {
+            userAge();
+        }
+    }
+
+    // if (gender === 'м' || gender === 'М') {
+    //     if (age >= 63) {
+    //         pension = 'да';
+    //     } else {
+    //         pension = 'нет';
+    //     }
+    // } else {
+    //     if (age >= 58) {
+    //         pension = 'да';
+    //     } else {
+    //         pension = 'нет';
+    //     }
+    // }
+
+    function userCard() {
+        userName();
+        userSurname();
+        userGender();
+        userAge();
+        return alert(`Полное имя: ${name} ${surname}
+           Пол: ${gender}
+           На пенсии: ${pension}`);
+    }
+    userCard();
 }());
